1ab911cee695d23317553b7bb7ed6a88b3486724
Joseph Zurowski <12567900+Dunklheit@users.noreply.github.com>
2020-08-26 16:57:49 -0400

    Update entry

---
+++ src/main/java/live/moonmoon/launcher/overlaypoc/ModInfo.java
@@ -0,0 +1,6 @@
+package live.moonmoon.launcher.overlaypoc;
+
+public final class ModInfo
+{
+  public static final String MODID = "overlay-poc";
+}
--- src/main/java/live/moonmoon/launcher/overlaypoc/Overlay.java
+++ src/main/java/live/moonmoon/launcher/overlaypoc/Overlay.java
@@ -2,35 +2,26 @@ package live.moonmoon.launcher.overlaypoc;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
-@Mod(
-  modid = Overlay.MOD_ID,
-  name = Overlay.MOD_NAME,
-  version = Overlay.VERSION,
-  clientSideOnly = true,
-  acceptedMinecraftVersions = "[1.12]"
-)
+@Mod(ModInfo.MODID)
 public class Overlay {
-  public static final String MOD_ID = "overlaypoc";
-  public static final String MOD_NAME = "OverlayPoc";
-  public static final String VERSION = "1.0-SNAPSHOT";
-
-  @Mod.Instance(MOD_ID)
-  public static Overlay INSTANCE;
-
   protected static ZContext ctx;
   protected static ZMQ.Socket client;
   protected static ZMQ.Socket server;
 
+  public Overlay() {
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+  }
+
   /**
    * This is the second initialization event. Register custom recipes
    */
-  @Mod.EventHandler
-  public void init(FMLInitializationEvent event) {
+  public void init(final FMLClientSetupEvent event) {
     System.out.println("Overlay Proof of Concept is starting...");
 
     ctx = new ZContext();
